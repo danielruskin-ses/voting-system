c73775646ef48f412ea092888d2dcab63f370499
Daniel Ruskin <daniel.ruskin@uconn.edu>
2018-12-04 01:42:50 +0000

    Vote Server Skeleton

---
+++ vote_server/README.md
@@ -0,0 +1,7 @@
+# Vote Server
+
+This folder contains the source code for the vote server.
+
+# Setup
+
+1. Install [GRPC C++](https://grpc.io/docs/quickstart/cpp.html)
---
+++ vote_server/vote_server.cc
@@ -0,0 +1,47 @@
+#include <grpc/grpc.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include <grpcpp/security/server_credentials.h>
+
+#include "vote_server.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReader;
+using grpc::ServerReaderWriter;
+using grpc::ServerWriter;
+using grpc::Status;
+
+class VoteServerImpl final : public VoteServer::Service {
+public:
+        explicit VoteServerImpl() {
+        }
+
+        Status GetElectionMetadata(ServerContext* context, const Empty* empty, ElectionMetadata* electionMetadata) override {
+                return Status::OK;
+        }
+
+        Status CastProposedBallot(ServerContext* context, const ProposedBallot* proposedBallot, RecordedBallot* recordedBallot) override {
+                return Status::OK;
+        }
+};
+
+void RunServer() {
+        std::string serverAddress("0.0.0.0:8001");
+        VoteServerImpl voteServer;
+
+        ServerBuilder builder;
+        builder.AddListeningPort(serverAddress, grpc::InsecureServerCredentials());
+        builder.RegisterService(&voteServer);
+
+        std::unique_ptr<Server> server(builder.BuildAndStart());
+        std::cout << "Server listening on: " << serverAddress << std::endl;
+        server->Wait();
+}
+
+int main(int argc, char** argv) {
+        RunServer();
+        return 0;
+}
